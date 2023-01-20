f00b59e1fb050598476b58d51c4273b74398d85c
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2023-01-20 11:27:30 -0700

    Initial Commit

---
+++ FigmaToCode/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FigmaToCode
+//
+//  Created by daviethdev on 1/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FigmaToCode/FigmaToCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaToCodeApp.swift
+//  FigmaToCode
+//
+//  Created by daviethdev on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FigmaToCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
