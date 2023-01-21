fa04a7f3abf58b450d4201a08fab8dcba538b29f
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2023-01-21 12:07:18 -0700

    Done - Created a new tab timer - Added coloring and Style

--- FigmaToCode/InboxView.swift
+++ FigmaToCode/InboxView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct InboxView: View {
+    @State var countDownTimer = 5
+    @State var timerRunning = false
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Color("Background")
+            Text("\(countDownTimer)")
+                .onReceive(timer) {_ in
+                    if countDownTimer > 0 && timerRunning {
+                        countDownTimer -= 1
+                    } else {
+                        timerRunning = false
+                    }
+                }
+                .font(.system(size: 80, weight: .bold))
+                .opacity(0.80)
+            
+            HStack(spacing: 30){
+                Button("Start") {
+                    timerRunning = true
+                }
+                
+                Button("Reset") {
+                    countDownTimer = 5
+                }.foregroundColor(.red)
+            }
+        }
     }
 }
 
